3fee51833eca04295163f27d13124782243b8f08
Samuel Cassiba <sam@cassiba.com>
2015-12-06 11:09:43 -0800

    Weather fixups, add wind and updated

--- plugins/weather.rb
+++ plugins/weather.rb
@@ -17,21 +17,24 @@ class Weather < IrbotPlugin
     cmd =~ /^(.)?(^w)|(weather)$/
   end
 
-  match(/(?:w) (.+)/, method: :weather, strip_colors: true)
+  match(/w (.+)/, method: :weather, strip_colors: true)
+  match(/weather (.+)/, method: :weather, strip_colors: true)
 
-  def weather(location)
+  def weather(m, location)
     if KEY
       wu = Wunderground.new(KEY)
-      hash wu.conditions_for(location)
+      hash = wu.conditions_for(location)
 
       if hash['current_observation']
-        location = hash['current_observation']['display_location']['full']
+        loc = hash['current_observation']['display_location']['full']
         weather = hash['current_observation']['weather']
         temp = hash['current_observation']['temperature_string']
-        humidity = hash['current_observations']['relative_humidity']
-        m.reply = "Weather for #{location}: #{weather} #{temp} Humidity: #{humidity}", true
+        humidity = hash['current_observation']['relative_humidity']
+        wind_str = hash['current_observation']['wind_string']
+        updated = Time.parse(hash['current_observation']['observation_time']).ago.to_words
+        m.reply "Weather for #{loc}: #{weather} #{temp} Humidity: #{humidity} Wind: #{wind_str} (updated #{updated})", true
       else
-        m.reply "Bad query for location \'#{location}\'.", true
+        m.reply "Bad query for location \'#{loc}\'.", true
       end
     else
       m.reply 'Internal error (missing API key)'
